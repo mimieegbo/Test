e217b3e6308a0414d6a13dd1772149f878b773aa
Abdulkabir Agboola <pro.kaiserlautern@gmail.com>
2022-03-31 16:02:41 +0100

    Hello, World

--- Polyarea.py
+++ Polyarea.py
@@ -1,14 +1,12 @@
+from shapes import Coordinate, Polygon
 
 
 def polyarea(x, y):
-    c1 = c2 = 0
-    for i in range(len(x)-1):
-        c1 += x[i]*y[i+1]
-        c2 += y[i]*x[i+1]
+    coordinates = []
+    for i in range(len(x)):
+        coordinates.append(Coordinate(x[i], y[i]))
 
-    c1 += x[-1]*y[0]
-    c2 += y[-1]*x[0]
+    return Polygon(*coordinates).area()
 
-    return 0.5*(abs(c1-c2))
 
 print(polyarea([1, 2], [12, 10]))
---
+++ experiment.py
@@ -0,0 +1,5 @@
+from shapes import Coordinate as C, Polygon as P
+
+Poly = P(C(1, 2), C(3, 4), C(5, 6))
+
+Poly.draw()
