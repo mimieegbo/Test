a9e67ba85f17313338681771b7b506a7aa42281d
Abdulkabir Agboola <pro.kaiserlautern@gmail.com>
2022-04-07 03:27:14 +0100

    Reconfigured the original setting of FirstBogusDB

--- Databases/database.py
+++ Databases/database.py
@@ -1,34 +1,61 @@
+from os import path
+from os_tools import mk, mkdir, rm
+
+location = None
+collection = None
+table = None
+
+
 class Database:
-    def __init__(self, url):
-        self.url = url
+    def __init__(self, location, collections=[]):
+        global collection, table
+
+        self.location = location
+
+        if not collections:
+            collections.add('Default')
+
+        self.collections = collections
+        mkdir(location)
+
+        for collection in collections:
+            mkdir(path.join(location, collection))
+
+        collection = collections['Default']
+
+class Collection:
+    def __init__(self, name):
+        self.name = name
+        self.location = path.join(location, name)
+        self.tables = set({})
+
+    def __del__(self):
+        rm(path.join(location, self.name))
 
     def createTable(self, name, datatype):
-        url = self.url
-        table_url = url + '/' + name
-        file = open(table_url, 'w+')
+        if not self.collections:
+            mkdir('default')
+        location = self.location
+        table_location = path.join(location, name)
+        file = open(table_location, 'w+')
         file.write('datatype = ' + datatype)
         file.close()
 
-        return Table(table_url, datatype)
+        return Table(table_location, datatype)
 
+    def showTables(self):
+        for i in self.tables:
+            print()
 
 class Table:
-    def __init__(self, table_url, columns):
-        self.url = table_url
+    def __init__(self, name, columns):
+        self.name = name
 
-    def __str__(self):
-        i = self.url.rindex('/')
-        name = self.url[i+1:]
-
-        return 'Table Object'
-
-    def f_three_lines(self):
-        file = open(self.url, 'r')
+    def exposee(self):
+        file = open(self.name, 'r')
         three_lines = []
 
         for i in range(3):
             three_lines.append(file.readline())
 
         return three_lines
-
-    def co
